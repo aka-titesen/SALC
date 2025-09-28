8b7a72c1a1f48df7ed80c32eab322503c4da220d
pablo <pablogastonmaidana123@gmail.com>
2025-09-28 01:03:49 -0300

    se re hizo el panel de medico

---
+++ SALC/Views/PanelMedico.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class PanelMedico
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ActionPanel = new System.Windows.Forms.Panel();
+            this.flowActions = new System.Windows.Forms.FlowLayoutPanel();
+            this.TxtBuscar = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.BtnBuscar = new System.Windows.Forms.Button();
+            this.BtnAgregar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnHistoricos = new System.Windows.Forms.Button();
+            this.dgvPacientes = new System.Windows.Forms.DataGridView();
+            this.nro_doc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tipo_documento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sexo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fecha_nacimiento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.direccion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.localidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.provincia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id_obra_social = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            this.ActionPanel.SuspendLayout();
+            this.flowActions.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPacientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1314, 60);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Panel Medico";
+            // 
+            // ActionPanel
+            // 
+            this.ActionPanel.BackColor = System.Drawing.Color.White;
+            this.ActionPanel.Controls.Add(this.flowActions);
+            this.ActionPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ActionPanel.Location = new System.Drawing.Point(0, 60);
+            this.ActionPanel.Name = "ActionPanel";
+            this.ActionPanel.Size = new System.Drawing.Size(1314, 60);
+            this.ActionPanel.TabIndex = 1;
+            // 
+            // flowActions
+            // 
+            this.flowActions.Controls.Add(this.TxtBuscar);
+            this.flowActions.Controls.Add(this.comboBox1);
+            this.flowActions.Controls.Add(this.BtnBuscar);
+            this.flowActions.Controls.Add(this.BtnAgregar);
+            this.flowActions.Controls.Add(this.btnEditar);
+            this.flowActions.Controls.Add(this.btnHistoricos);
+            this.flowActions.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowActions.Location = new System.Drawing.Point(0, 0);
+            this.flowActions.Name = "flowActions";
+            this.flowActions.Padding = new System.Windows.Forms.Padding(10);
+            this.flowActions.Size = new System.Drawing.Size(1314, 60);
+            this.flowActions.TabIndex = 0;
+            this.flowActions.WrapContents = false;
+            // 
+            // TxtBuscar
+            // 
+            this.TxtBuscar.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtBuscar.Location = new System.Drawing.Point(13, 13);
+            this.TxtBuscar.Name = "TxtBuscar";
+            this.TxtBuscar.Size = new System.Drawing.Size(260, 27);
+            this.TxtBuscar.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(279, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // BtnBuscar
+            // 
+            this.BtnBuscar.AutoSize = true;
+            this.BtnBuscar.BackColor = System.Drawing.Color.DodgerBlue;
+            this.BtnBuscar.FlatAppearance.BorderSize = 0;
+            this.BtnBuscar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.BtnBuscar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.BtnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnBuscar.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnBuscar.ForeColor = System.Drawing.Color.White;
+            this.BtnBuscar.Location = new System.Drawing.Point(408, 15);
+            this.BtnBuscar.Margin = new System.Windows.Forms.Padding(5);
+            this.BtnBuscar.Name = "BtnBuscar";
+            this.BtnBuscar.Size = new System.Drawing.Size(82, 30);
+            this.BtnBuscar.TabIndex = 7;
+            this.BtnBuscar.Text = "Buscar";
+            this.BtnBuscar.UseVisualStyleBackColor = false;
+            // 
+            // BtnAgregar
+            // 
+            this.BtnAgregar.AutoSize = true;
+            this.BtnAgregar.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.BtnAgregar.FlatAppearance.BorderSize = 0;
+            this.BtnAgregar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.BtnAgregar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.BtnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnAgregar.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAgregar.ForeColor = System.Drawing.Color.White;
+            this.BtnAgregar.Location = new System.Drawing.Point(500, 15);
+            this.BtnAgregar.Margin = new System.Windows.Forms.Padding(5);
+            this.BtnAgregar.Name = "BtnAgregar";
+            this.BtnAgregar.Size = new System.Drawing.Size(137, 30);
+            this.BtnAgregar.TabIndex = 8;
+            this.BtnAgregar.Text = "Agregar Paciente";
+            this.BtnAgregar.UseVisualStyleBackColor = false;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.AutoSize = true;
+            this.btnEditar.BackColor = System.Drawing.Color.SlateGray;
+            this.btnEditar.FlatAppearance.BorderSize = 0;
+            this.btnEditar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnEditar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditar.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.ForeColor = System.Drawing.Color.White;
+            this.btnEditar.Location = new System.Drawing.Point(647, 15);
+            this.btnEditar.Margin = new System.Windows.Forms.Padding(5);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(146, 30);
+            this.btnEditar.TabIndex = 9;
+            this.btnEditar.Text = "Editar Informacion";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            // 
+            // btnHistoricos
+            // 
+            this.btnHistoricos.AutoSize = true;
+            this.btnHistoricos.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnHistoricos.FlatAppearance.BorderSize = 0;
+            this.btnHistoricos.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnHistoricos.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray;
+            this.btnHistoricos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHistoricos.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHistoricos.ForeColor = System.Drawing.Color.White;
+            this.btnHistoricos.Location = new System.Drawing.Point(803, 15);
+            this.btnHistoricos.Margin = new System.Windows.Forms.Padding(5);
+            this.btnHistoricos.Name = "btnHistoricos";
+            this.btnHistoricos.Size = new System.Drawing.Size(102, 30);
+            this.btnHistoricos.TabIndex = 10;
+            this.btnHistoricos.Text = "Ver estudios";
+            this.btnHistoricos.UseVisualStyleBackColor = false;
+            this.btnHistoricos.Click += new System.EventHandler(this.btnHistoricos_Click);
+            // 
+            // dgvPacientes
+            // 
+            this.dgvPacientes.AllowUserToAddRows = false;
+            this.dgvPacientes.AllowUserToDeleteRows = false;
+            this.dgvPacientes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvPacientes.BackgroundColor = System.Drawing.Color.White;
+            this.dgvPacientes.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.DodgerBlue;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvPacientes.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvPacientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPacientes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nro_doc,
+            this.tipo_documento,
+            this.nombre,
+            this.apellido,
+            this.sexo,
+            this.fecha_nacimiento,
+            this.direccion,
+            this.localidad,
+            this.provincia,
+            this.telefono,
+            this.mail,
+            this.id_obra_social});
+            this.dgvPacientes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvPacientes.EnableHeadersVisualStyles = false;
+            this.dgvPacientes.Location = new System.Drawing.Point(0, 120);
+            this.dgvPacientes.MultiSelect = false;
+            this.dgvPacientes.Name = "dgvPacientes";
+            this.dgvPacientes.ReadOnly = true;
+            this.dgvPacientes.RowHeadersWidth = 51;
+            this.dgvPacientes.RowTemplate.Height = 24;
+            this.dgvPacientes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPacientes.Size = new System.Drawing.Size(1314, 439);
+            this.dgvPacientes.TabIndex = 2;
+            // 
+            // nro_doc
+            // 
+            this.nro_doc.HeaderText = "Nro. Doc";
+            this.nro_doc.MinimumWidth = 6;
+            this.nro_doc.Name = "nro_doc";
+            this.nro_doc.ReadOnly = true;
+            // 
+            // tipo_documento
+            // 
+            this.tipo_documento.HeaderText = "Tipo Doc.";
+            this.tipo_documento.MinimumWidth = 6;
+            this.tipo_documento.Name = "tipo_documento";
+            this.tipo_documento.ReadOnly = true;
+            // 
+            // nombre
+            // 
+            this.nombre.HeaderText = "Nombre";
+            this.nombre.MinimumWidth = 6;
+            this.nombre.Name = "nombre";
+            this.nombre.ReadOnly = true;
+            // 
+            // apellido
+            // 
+            this.apellido.HeaderText = "Apellido";
+            this.apellido.MinimumWidth = 6;
+            this.apellido.Name = "apellido";
+            this.apellido.ReadOnly = true;
+            // 
+            // sexo
+            // 
+            this.sexo.HeaderText = "Sexo";
+            this.sexo.MinimumWidth = 6;
+            this.sexo.Name = "sexo";
+            this.sexo.ReadOnly = true;
+            // 
+            // fecha_nacimiento
+            // 
+            this.fecha_nacimiento.HeaderText = "Nacimiento";
+            this.fecha_nacimiento.MinimumWidth = 6;
+            this.fecha_nacimiento.Name = "fecha_nacimiento";
+            this.fecha_nacimiento.ReadOnly = true;
+            // 
+            // direccion
+            // 
+            this.direccion.HeaderText = "Direccion";
+            this.direccion.MinimumWidth = 6;
+            this.direccion.Name = "direccion";
+            this.direccion.ReadOnly = true;
+            // 
+            // localidad
+            // 
+            this.localidad.HeaderText = "Localidad";
+            this.localidad.MinimumWidth = 6;
+            this.localidad.Name = "localidad";
+            this.localidad.ReadOnly = true;
+            // 
+            // provincia
+            // 
+            this.provincia.HeaderText = "Provincia";
+            this.provincia.MinimumWidth = 6;
+            this.provincia.Name = "provincia";
+            this.provincia.ReadOnly = true;
+            // 
+            // telefono
+            // 
+            this.telefono.HeaderText = "Telefono";
+            this.telefono.MinimumWidth = 6;
+            this.telefono.Name = "telefono";
+            this.telefono.ReadOnly = true;
+            // 
+            // mail
+            // 
+            this.mail.HeaderText = "Email";
+            this.mail.MinimumWidth = 6;
+            this.mail.Name = "mail";
+            this.mail.ReadOnly = true;
+            // 
+            // id_obra_social
+            // 
+            this.id_obra_social.HeaderText = "Obra Social";
+            this.id_obra_social.MinimumWidth = 6;
+            this.id_obra_social.Name = "id_obra_social";
+            this.id_obra_social.ReadOnly = true;
+            // 
+            // PanelMedico
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1314, 559);
+            this.Controls.Add(this.dgvPacientes);
+            this.Controls.Add(this.ActionPanel);
+            this.Controls.Add(this.panel1);
+            this.Name = "PanelMedico";
+            this.Text = "Medico";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ActionPanel.ResumeLayout(false);
+            this.flowActions.ResumeLayout(false);
+            this.flowActions.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPacientes)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel ActionPanel;
+        private System.Windows.Forms.FlowLayoutPanel flowActions;
+        private System.Windows.Forms.TextBox TxtBuscar;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button BtnBuscar;
+        private System.Windows.Forms.Button BtnAgregar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnHistoricos;
+        private System.Windows.Forms.DataGridView dgvPacientes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nro_doc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tipo_documento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn apellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sexo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fecha_nacimiento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn direccion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn localidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn provincia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telefono;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id_obra_social;
+    }
+}
---
+++ SALC/Views/PanelMedico.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class PanelMedico : Form
+    {
+        public PanelMedico()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHistoricos_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            var f = new HistorialEstudiosForm();
+            f.ShowDialog(this);
+
+            this.Show();
+        }
+    }
+}
