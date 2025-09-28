11853e124204ffe3ad043edf1792223194cb8d4e
pablo <pablogastonmaidana123@gmail.com>
2025-09-28 01:05:21 -0300

    se hizo el panel de historial medico

---
+++ SALC/Views/HistorialEstudiosForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class HistorialEstudiosForm : Form
+    {
+        public HistorialEstudiosForm()
+        {
+            InitializeComponent();
+        }
+
+        private void HistorialEstudiosForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTitulo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnVolver_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
